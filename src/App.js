f86c811a349f2ea00985033dcba8ec7760774069
ilya1989 <ilyakolker@gmail.com>
2018-11-14 22:41:33 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import Person from './Person/Person';
+import './App.css';
+
+
+class App extends Component {
+
+  state = {
+    persons:[
+      {name: "ilya", age:29},
+      {name: "orly", age:28},
+      {name: "ruslan", age:27}
+    ]
+    
+  }
+  
+  SwitchHandler = (newName)=>{
+    this.setState({
+      persons:[
+        {name: newName, age:29},
+        {name: "orly", age:28},
+        {name: "ruslan", age:30}
+      ]
+    })
+  }
+
+SwitchHandlerInput = (e) => {
+  this.setState({
+    persons:[
+      {name: "ilya", age:29},
+      {name: e.target.value, age:28},
+      {name: "ruslan", age:27}
+    ]
+  })
+}
+
+  render() {
+
+  const  style ={
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    return (
+      <div className="App">
+      <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age}
+        title="test header"/>
+      <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age}
+        click={this.SwitchHandler.bind(this, "Da king")}
+        changed={this.SwitchHandlerInput}>Wow this is true</Person>
+      <Person 
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age}/>
+      <button 
+      style={style}
+      onClick={()=>this.SwitchHandler("kolker")}>change props</button>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Person.css';
+const person = (props) => {
+    return(
+        <div className="Person">
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old</p>
+            <h1>{props.title}</h1>
+            <p>{props.children}</p>
+            <input type='text' onChange={props.changed} value={props.name}/>
+        </div>
+    )
+}
+
+export default person;
