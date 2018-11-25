787ecdd233685cbf92f6c8aeaa744ba1a14489c6
ilyakolker <ilyakolker@gmail.com>
2018-11-25 12:22:31 +0200

    leson 52 -> dynamic lists using map function

--- src/App.js
+++ src/App.js
@@ -7,32 +7,33 @@ class App extends Component {
 
   state = {
     persons:[
-      {name: "ilya", age:29},
-      {name: "orly", age:28},
-      {name: "ruslan", age:27}
-    ]
-    
-  }
-  
-  SwitchNameHandler = (newName)=>{
-    this.setState({
-      persons:[
-        {name: newName, age:29},
-        {name: "orly", age:28},
-        {name: "ruslan", age:30}
-      ]
-    })
-  }
-
-SwitchHandlerInput = (e) => {
-  this.setState({
-    persons:[
-      {name: "ilya", age:29},
-      {name: e.target.value, age:28},
-      {name: "ruslan", age:27}
+      {id:'1',name: "ilya", age:29},
+      {id:'2',name: "orly", age:28},
+      {id:'3',name: "ruslan", age:27}
     ],
     showPersons : false,
-  })
+    
+  }
+deletePersonHandler = (personIndex) =>{
+const persons = [...this.state.persons];
+persons.splice(personIndex,1);
+this.setState({
+  persons: persons
+});
+}
+
+SwitchHandlerInput = (e,id) => {
+  const personIndex = this.state.persons.findIndex(p => {
+    return p.id === id;
+  });
+  const person = {
+    ...this.state.persons[personIndex]
+  };
+
+  person.name = e.target.value;
+  const persons = [...this.state.persons];
+  persons[personIndex] = person;
+  this.setState({persons:persons});
 }
 
 togglePersonHandler = () => {
@@ -52,30 +53,30 @@ togglePersonHandler = () => {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if(this.state.showPersons){
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person 
+                      click={() => this.deletePersonHandler(index)}
+                      name={person.name}
+                      age={person.age}
+                      key={person.id}
+                      changed={(event) => this.SwitchHandlerInput(event,person.id)} />
+          })}
+        </div> 
+      );
+    }
+
     return (
       <div className="App">
-     { 
-       !this.state.showPersons ?
-        <div>
-            <Person 
-              name={this.state.persons[0].name} 
-              age={this.state.persons[0].age}
-              title="testing header"/>
-            <Person 
-              name={this.state.persons[1].name} 
-              age={this.state.persons[1].age}
-              click={this.SwitchNameHandler.bind(this, "Da king")}// Better to use bind 
-              changed={this.SwitchHandlerInput}
-              >Wow this is true</Person>
-            <Person 
-              name={this.state.persons[2].name} 
-              age={this.state.persons[2].age}/>
-        </div> : null
-      }
       <button 
       style={style}
-      onClick={this.togglePersonHandler}//not a good practice to use method
-      >change props</button>
+      onClick={this.togglePersonHandler}
+      >Toggle persons</button>
+      {persons}
       </div>
     );
   }
